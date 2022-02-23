774ff22988f388b96f29c6a7df2eeaf3a44674af
numbmartin <xxw603@miami.edu>
2022-02-23 16:50:20 -0500

    lalala

---
+++ js/test.js
@@ -0,0 +1,25 @@
+alert("this is awesome")
+console.log("what is up")
+
+console.log(7+4)
+
+const myVariable= "this is my first variable"
+
+const myNumber= 1 
+
+console.log("4" + 3)
+
+var thisVar= 8
+var thisVar= 10
+
+let otherVar= 4
+otherVar= 6
+
+// , saves you time to type multiple consts
+const firstVar= 1,   
+secondVar =2, 
+thirdVar = 3 
+
+const year = "2022"
+console.log("this year is " + year)
+
