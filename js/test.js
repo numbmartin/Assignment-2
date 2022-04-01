0d4165e09c2a389f408e534b1d6ccc4f9d44c31e
numbmartin <xxw603@miami.edu>
2022-04-01 10:30:14 -0400

    sss

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 // alert("this is awesome")
-console.log("what is up")
+console.log("como estas")
 
 console.log(7+4)
 
@@ -7,7 +7,7 @@ const myVariable= "this is my first variable"
 
 const myNumber= 1 
 
-console.log("4" + 3)
+console.log("4" + " " + "3")
 
 var thisVar= 8
 var thisVar= 10
@@ -78,5 +78,87 @@ const item2Po = myArrayLong.indexOf("item2")
 console.log(item2Po)
 
 myArrayLong.splice(2,0,"lalallala")
+
 console.log(myArrayLong)
 
+console.log(4+3)
+
+let num1 = 30
+let num2 = 50
+num2 = 80 
+const add= num1 + num2
+console.log(add)
+
+
+
+
+function myAddFunction(){
+
+    4+3
+}
+
+function myMultiplyFunction(){
+
+    4*3
+}
+
+console.log(myAddFunction)
+console.log(myMultiplyFunction)
+
+function toast(topping){
+
+    console.log(topping + " is expensive")
+}
+
+toast("truffle")
+
+function Dozon(){
+
+    return "6"+"3"
+}
+
+console.log(mySecondFunction())
+
+const myDog = {
+    name: 'Bruno',
+    color: 'dark',
+    breed: 'golden retriver',
+    weight: '130lb',
+    hobby: "walk on the beach"
+     }
+
+
+let num = 5
+
+if (num<10) {
+    console.log("iam small")
+}
+
+else{
+    console.log("im big")
+}
+
+
+const fruitCount = {
+    apple: 28,
+    orange: 17,
+    pear: 54,
+    pineapple: 10,
+    banana: 2
+    }
+    const fruitKeys = Object.keys(fruitValue)
+    console.log(fruitValue)
+
+
+    const numberList = [2, ,456, 34, 100, 59, 75, 200, 970];
+    
+     for (let i = 0; i < numberList.length; i++) {
+     // this variable get the value of each element of the array
+     const num = numberList[i]
+     if (num < 75) {
+     console.log(num + ' is smaller than 75')
+     }
+     else{
+     console.log(num + ' is bigger than 75')
+     }
+     }
