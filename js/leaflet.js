cce1cde1104a2f80f552b3122f5c3097d664d020
numbmartin <xxw603@miami.edu>
2022-04-01 16:49:16 -0400

    2222

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,9 +1,16 @@
-var map = L.map('map').setView([25.7825453, -80.2994984], 17);
+var map = L.map('map').setView([25.7178972,-80.2768255], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([25.7825453, -80.2994984]).addTo(map)
+L.marker([25.7178972,-80.2768255]).addTo(map)
     .bindPopup('My Sweet Home.')
     .openPopup();
+
+    var circle = L.circle([25.7178972,-80.2768255], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 100
+    }).addTo(map);
